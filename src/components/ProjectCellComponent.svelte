93945ee744e88d0644009da266696602987a368d
Darren Sim <45655404+Kalsyc@users.noreply.github.com>
2022-01-05 16:05:32 +0800

    Refactor project preview in page to separate component

---
+++ src/components/ProjectCellComponent.svelte
@@ -0,0 +1,177 @@
+<script lang="ts">
+  import { url } from '@roxi/routify';
+
+  export let projectParam: string;
+  export let projectImgPath: string;
+  export let projectTitle: string;
+  export let projectDesc: string;
+  export let projectType: string;
+  export let projectTag: string;
+</script>
+
+<div class="completed-cell">
+  <a href={$url('/projects/:project', { project: projectParam })}>
+    <img class="completed-img" alt="" src={projectImgPath} />
+    <div class="completed-overlay">
+      <div class="completed-tag {projectType}">{projectTag}</div>
+      <div class="completed-text">
+        <div class="completed-title">
+          {projectTitle}
+        </div>
+        <div class="completed-desc">
+          {projectDesc}
+        </div>
+      </div>
+    </div>
+  </a>
+</div>
+
+<style>
+  .completed-cell {
+    margin: 0.5em;
+    position: relative;
+  }
+
+  .completed-cell:hover {
+    cursor: pointer;
+  }
+
+  .completed-cell:hover .completed-overlay {
+    opacity: 1;
+  }
+
+  .completed-overlay {
+    border-radius: 1em;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100%;
+    width: 100%;
+    opacity: 0;
+    transition: 0.3s ease-in;
+    background-color: rgba(0, 0, 0, 0.6);
+  }
+
+  .completed-img {
+    display: block;
+    height: 225px;
+    width: 400px;
+    aspect-ratio: 1;
+    object-fit: cover;
+    border-radius: 1em;
+  }
+
+  .completed-tag {
+    position: absolute;
+    top: 3%;
+    left: 3%;
+    color: white;
+    font-weight: 700;
+    border-radius: 10px;
+    padding: 0 0.5em;
+    width: fit-content;
+  }
+
+  .completed-text {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+
+  .completed-title,
+  .completed-desc {
+    color: white;
+    font-weight: 700;
+    font-size: 1.25rem;
+    padding: 0 0.75em;
+  }
+
+  .webdev {
+    background-color: #26a400;
+  }
+
+  .uiux {
+    background-color: #e87121;
+  }
+
+  .gamedev {
+    background-color: #0118e2;
+  }
+
+  .appdev {
+    background-color: #e70013;
+  }
+
+  .vr {
+    background-color: #7000ad;
+  }
+
+  @media screen and (max-width: 900px) {
+    .completed-overlay {
+      top: 50%;
+      height: 50%;
+      opacity: 1;
+      display: flex;
+      flex-direction: column;
+    }
+
+    .completed-text {
+      align-items: flex-start;
+      justify-content: center;
+    }
+
+    .completed-title,
+    .completed-desc {
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+      font-size: 1rem;
+    }
+
+    .completed-tag {
+      top: 0;
+      left: 0;
+      border-radius: 10px 0 0 0;
+      position: relative;
+      width: fit-content;
+    }
+  }
+
+  @media screen and (max-width: 600px) {
+    .completed-img {
+      height: 40vw;
+      width: 80vw;
+    }
+    .completed-tag {
+      position: relative;
+      text-align: left;
+      max-width: fit-content;
+    }
+
+    .completed-title,
+    .completed-desc {
+      text-align: left;
+    }
+  }
+
+  @media screen and (max-width: 400px) {
+    .completed-tag {
+      font-size: 0.75rem;
+    }
+
+    .completed-title {
+      font-size: 0.8rem;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+    }
+
+    .completed-desc {
+      display: none;
+    }
+  }
+</style>
--- src/pages/projects/index.svelte
+++ src/pages/projects/index.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import HeaderComponent from '../../components/HeaderComponent.svelte';
   import FooterComponent from '../../components/FooterComponent.svelte';
-  import { url, metatags } from '@roxi/routify';
+  import { metatags } from '@roxi/routify';
+  import ProjectCellComponent from '../../components/ProjectCellComponent.svelte';
 
   metatags.title = 'Simply Darren | Projects';
 </script>
@@ -13,142 +14,84 @@
     <div class="projects-completed">
       <span class="completed-title">Completed Projects</span>
       <div class="completed-grid">
-        <div class="completed-cell">
-          <a href={$url('/projects/:project', { project: 'PetSaver' })}>
-            <img class="completed-img" alt="" src="/img/petsaver-img.jpeg" />
-            <div class="completed-overlay">
-              <div class="completed-tag uiux-tag">UX/UI Design</div>
-              <div class="completed-text">
-                PetSaver
-                <br />
-                Personal Savings App for UX Design Student Project
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="completed-cell">
-          <a href={$url('/projects/:project', { project: 'ProjectKampong' })}>
-            <img class="completed-img " alt="" src="/img/kampong-img.png" />
-            <div class="completed-overlay">
-              <div class="completed-tag webdev-tag ">Web Dev</div>
-              <div class="completed-text">
-                Project Kampong
-                <br />
-                An online collaborative platform for the Social Sector
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="completed-cell">
-          <a href={$url('/projects/:project', { project: 'DigitalKampung' })}>
-            <img class="completed-img" alt="" src="/img/digitalkampung-img.png" />
-            <div class="completed-overlay">
-              <div class="completed-tag webdev-tag">Web Dev</div>
-              <div class="completed-text">
-                Digital Kampung
-                <br />
-                Search, Review and Rate Hawker stalls across Singapore!
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="completed-cell">
-          <a href={$url('/projects/cull1')}>
-            <img class="completed-img" alt="" src="/img/cullinary-gif.gif" />
-            <div class="completed-overlay">
-              <div class="completed-tag gamedev-tag">Game Dev</div>
-              <div class="completed-text">
-                CULLinary
-                <br />
-                Student Game Project, 1st Place at 18th iSTePS
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="completed-cell">
-          <a href={$url('/projects/cull2')}>
-            <img class="completed-img" alt="" src="/img/cullinary2-gif.gif" />
-            <div class="completed-overlay ">
-              <div class="completed-tag gamedev-tag">Game Dev</div>
-              <div class="completed-text">
-                CULLinary2
-                <br />
-                Student Game Project, 1st Place at 19th iSTePS
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="completed-cell">
-          <a href={$url('/projects/:project', { project: 'SafeSpace' })}>
-            <img class="completed-img" alt="" src="/img/safespace-img.png" />
-            <div class="completed-overlay">
-              <div class="completed-tag vr-tag">Virtual Reality</div>
-              <div class="completed-text">
-                SafeSpace
-                <br />
-                VR Mental Health Simulator and Meditation Guide
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="completed-cell">
-          <a href={$url('/projects/:project', { project: 'Xpire' })}>
-            <img class="completed-img" alt="" src="/img/xpire-img.png" />
-            <div class="completed-overlay ">
-              <div class="completed-tag appdev-tag">App Dev</div>
-              <div class="completed-text">
-                Xpire
-                <br />
-                Desktop Expiry Date Tracker Application written in Java
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="completed-cell">
-          <a href={$url('/projects/:project', { project: 'CanMakan' })}>
-            <img class="completed-img" alt="" src="/img/canmakan.jpg" />
-            <div class="completed-overlay">
-              <div class="completed-tag webdev-tag">Web Dev</div>
-              <div class="completed-text">
-                CanMakan
-                <br />
-                Website that checks dine-in rules for vaccinated people
-              </div>
-            </div>
-          </a>
-        </div>
+        <ProjectCellComponent
+          projectType="uiux"
+          projectParam="PetSaver"
+          projectImgPath="/img/petsaver-img.jpeg"
+          projectTitle="PetSaver"
+          projectDesc="Personal Savings App for UX Design Project"
+          projectTag="UI/UX Design"
+        />
+        <ProjectCellComponent
+          projectType="webdev"
+          projectParam="ProjectKampong"
+          projectImgPath="/img/kampong-img.png"
+          projectTitle="Project Kampong"
+          projectDesc="Online Collaborative platform for Social Sector"
+          projectTag="Web Dev"
+        />
+        <ProjectCellComponent
+          projectType="gamedev"
+          projectParam="CULLinary"
+          projectImgPath="/img/cullinary-gif.gif"
+          projectTitle="CULLinary"
+          projectDesc="Dungeon Crawler and Restaurant Simulator"
+          projectTag="Game Dev"
+        />
+        <ProjectCellComponent
+          projectType="gamedev"
+          projectParam="CULLinary2"
+          projectImgPath="/img/cullinary2-gif.gif"
+          projectTitle="CULLinary2"
+          projectDesc="Dungeon Crawler and Delivery Simulator"
+          projectTag="Game Dev"
+        />
+        <ProjectCellComponent
+          projectType="vr"
+          projectParam="SafeSpace"
+          projectImgPath="/img/safespace-img.png"
+          projectTitle="SafeSpace"
+          projectDesc="VR Mental Health Simulator and Meditation Guide"
+          projectTag="Virtual Reality"
+        />
+        <ProjectCellComponent
+          projectType="appdev"
+          projectParam="Xpire"
+          projectImgPath="/img/xpire-img.png"
+          projectTitle="Xpire"
+          projectDesc="Desktop Expiry Date Tracker Application"
+          projectTag="App Dev"
+        />
+        <ProjectCellComponent
+          projectType="webdev"
+          projectParam="CanMakan"
+          projectImgPath="/img/canmakan.jpg"
+          projectTitle="CanMakan"
+          projectDesc="Dine-in Rules Checker"
+          projectTag="Web Dev"
+        />
       </div>
     </div>
 
     <div class="projects-ongoing">
       <span class="ongoing-title">Ongoing Projects</span>
       <div class="completed-grid">
-        <div class="completed-cell">
-          <a href={$url('/projects/:project', { project: 'Yobu' })}>
-            <img class="completed-img" alt="" src="/img/yobu.jpg" />
-            <div class="completed-overlay">
-              <div class="completed-tag webdev-tag">Web Dev</div>
-              <div class="completed-text">
-                Yobu
-                <br />
-                Social Gaming Platform - The newest way to connect
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="completed-cell">
-          <a href={$url('/projects/:project', { project: 'SayNoToUILibraries' })}>
-            <img class="completed-img" alt="" src="/img/saynotouilibraries.jpg" />
-            <div class="completed-overlay">
-              <div class="completed-tag webdev-tag">Web Dev</div>
-              <div class="completed-text">
-                SayNoToUILibraries
-                <br />
-                Houses all my Frontend Components and UI Elements
-              </div>
-            </div>
-          </a>
-        </div>
+        <ProjectCellComponent
+          projectType="webdev"
+          projectParam="Yobu"
+          projectImgPath="/img/yobu.jpg"
+          projectTitle="Yobu"
+          projectDesc="Social Gaming Platform - The newest way to connect"
+          projectTag="Web Dev"
+        />
+        <ProjectCellComponent
+          projectType="webdev"
+          projectParam="SayNoToUILibraries"
+          projectImgPath="/img/saynotouilibraries.jpg"
+          projectTitle="SayNoToUILibraries"
+          projectDesc="UI Component Reference & Storage"
+          projectTag="Web Dev"
+        />
       </div>
     </div>
   </div>
@@ -168,84 +111,6 @@
     padding: 1em 0;
     display: grid;
     grid-template-columns: repeat(3, 1fr);
-  }
-
-  .completed-cell {
-    margin: 0.5em;
-    position: relative;
-  }
-
-  .completed-cell:hover {
-    cursor: pointer;
-  }
-
-  .completed-cell:hover .completed-overlay {
-    opacity: 1;
-  }
-
-  .completed-img {
-    display: block;
-    height: 225px;
-    width: 400px;
-    aspect-ratio: 1;
-    object-fit: cover;
-    border-radius: 1em;
-  }
-
-  .completed-overlay {
-    border-radius: 1em;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    height: 100%;
-    width: 100%;
-    opacity: 0;
-    transition: 0.3s ease-in;
-    background-color: rgba(0, 0, 0, 0.6);
-  }
-
-  .completed-tag {
-    position: absolute;
-    top: 3%;
-    left: 3%;
-    color: white;
-    font-weight: 700;
-    border-radius: 10px;
-    padding: 0 0.5em;
-  }
-
-  .webdev-tag {
-    background-color: #26a400;
-  }
-
-  .uiux-tag {
-    background-color: #e87121;
-  }
-
-  .gamedev-tag {
-    background-color: #0118e2;
-  }
-
-  .appdev-tag {
-    background-color: #e70013;
-  }
-
-  .vr-tag {
-    background-color: #7000ad;
-  }
-
-  .completed-text {
-    width: auto;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: white;
-    font-weight: 700;
-    font-size: 1.25rem;
-    padding: 0 1em;
   }
 
   .completed-title,
@@ -282,40 +147,6 @@
   @media screen and (max-width: 900px) {
     .completed-grid {
       grid-template-columns: repeat(1, 1fr);
-    }
-
-    .completed-overlay {
-      top: 50%;
-      height: 50%;
-      opacity: 1;
-    }
-
-    .completed-text {
-      text-align: left;
-      font-size: 1rem;
-      justify-content: flex-start;
-    }
-  }
-
-  @media screen and (max-width: 600px) {
-    .completed-img {
-      height: 40vw;
-      width: 80vw;
-    }
-    .completed-tag {
-      position: relative;
-      text-align: left;
-      max-width: 35%;
-      text-overflow: ellipsis;
-      overflow: hidden;
-      white-space: nowrap;
-    }
-  }
-
-  @media screen and (max-width: 400px) {
-    .completed-text,
-    .completed-tag {
-      font-size: 0.75rem;
     }
   }
 </style>
