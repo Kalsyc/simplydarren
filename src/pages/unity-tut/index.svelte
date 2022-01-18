61b2b1cdf6299d2470f63726d1c90dcea1a84953
Darren Sim <45655404+Kalsyc@users.noreply.github.com>
2022-01-18 13:55:34 +0800

    Add in unity tutorial for blog

--- src/pages/unity-tut/index.svelte
+++ src/pages/unity-tut/index.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-  import HeaderComponent from '../components/HeaderComponent.svelte';
-  import FooterComponent from '../components/FooterComponent.svelte';
+  import HeaderComponent from '../../components/HeaderComponent.svelte';
+  import FooterComponent from '../../components/FooterComponent.svelte';
   import { metatags, url } from '@roxi/routify';
 
-  metatags.title = 'Simply Darren | Blog';
+  metatags.title = 'Simply Darren | Unity Tutorials';
 </script>
 
 <main>
   <HeaderComponent />
   <div class="blog-main-div">
-    <a href={$url('/unity-tut')}><span class="blog-title-div">Unity Tutorials</span></a>
-    <span class="blog-title-div">Latest Blog - Coming Soon :)</span>
+    <span class="blog-title-div">For CS4240 S2 AY2122</span>
+    <span class="blog-title-div">Useful Guides</span>
   </div>
   <FooterComponent />
 </main>
